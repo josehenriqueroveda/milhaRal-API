dacb29c21977bf66b1a7158008bcacc8ac86bd49
Jose Henrique Roveda <jroveda@lpht.com.br>
2021-08-08 16:37:44 -0300

    MSegundo Método - Illinois

--- api.R
+++ api.R
@@ -1,6 +1,6 @@
 # Installing and loading the packages
 
-pacotes <- c("plumber","gapminder","roxygen2")
+pacotes <- c("plumber","gapminder","roxygen2", "Rook")
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
   instalador <- pacotes[!pacotes %in% installed.packages()]
@@ -28,4 +28,17 @@ if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
 function(peso_medio, populacao) {
   res <- (as.numeric(peso_medio)/1000) * as.numeric(populacao)
   return(list(Produtividade_Estimada = res, Unidade_Medida = "kg/ha"))
+}
+
+# Segundo Método
+
+#* Corn Yield Calculation
+#* 
+#* @param req:file Arquivo CSV das amostras coletadas
+#* @post /illinois
+function(req) {
+  file <- Rook::Multipart$parse(req)$req$tempfile
+  amostras <- read.csv(file)
+  amostras$Prod_Espiga <- (amostras$Espigas4m2) * (amostras$FileirasGraos) * (amostras$GraosFileira) * 0.70
+  mean(amostras$Prod_Espiga)
 }
