8b004be4af063f6ee02f722712ce93fdcfcf875f
Jose Henrique Roveda <jroveda@lpht.com.br>
2021-08-09 14:57:07 -0300

    Swagger Doc

--- api.R
+++ api.R
@@ -14,13 +14,16 @@ if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
 
 # -----------------------------------------------------------------------------
 #* @apiTitle milhaRal API
-#* @apiDescription API para cálculos de produtividade do milho.
+#* @apiDescription API para estimar a produtividade do milho.
+#* @apiContact list(name = "José Henrique Roveda", url = "https://www.linkedin.com/in/jhroveda/", email = "josehenriqueroveda@usp.br")
+#* @apiLicense list(name = "MIT License", url = "https://raw.githubusercontent.com/josehenriqueroveda/milhaRal-API/main/LICENSE")
+#* @apiTag Estimativas Cálculos para estimar a produtividade do milho
 
 # -----------------------------------------------------------------------------
 # Primeiro Método
 
 #* Cálculo de Produtividade - Simples
-#* 
+#* @tag Estimativas
 #* @param peso_medio Peso médio dos grãos (g) das espigas selecionadas
 #* @param populacao População de plantas na área por hectare
 #* @serializer unboxedJSON
@@ -33,21 +36,21 @@ function(peso_medio, populacao) {
 # Segundo Método
 
 #* Corn Yield Calculation - Universidade de Illinois
-#* 
+#* @tag Estimativas
 #* @param req:file Arquivo CSV das amostras coletadas
 #* @post /illinois
 function(req) {
   file <- Rook::Multipart$parse(req)$req$tempfile
   amostras <- read.csv(file)
   amostras$Prod_Espiga <- (amostras$Espigas4m2) * (amostras$FileirasGraos) * (amostras$GraosFileira) * 0.70
-  list(Produtividade_Estimada = mean(amostras$Prod_Espiga), Unidade_Medida = "kg/ha a 15.5% de umidade")
+  return(list(Produtividade_Estimada = mean(amostras$Prod_Espiga), Unidade_Medida = "kg/ha a 15.5% de umidade"))
 }
 
 
 # Terceiro Método
 
 #* Cáculo de Produtividade - Emater
-#* 
+#* @tag Estimativas
 #* @param n_espigas Número de espigas em 10m lineares
 #* @param peso_medio Peso médio de grãos de 3 espigas coletadas (g)
 #* @param entre_linhas Espaçamento entre linhas (m)
